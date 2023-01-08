95a09c333dc913193c2db07a8704b9a9b783739b
Kimo <kimo@ya.code>
2023-01-08 17:58:22 +0300

    Added set function

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-def getMax(list):
-    print(max(list))
+def setList():
+    n = int(input('Amount of elements: '))
+    ilist = []
+    for i in range (n):
+        ilist.append(int(input(f'Enter the number ({n} times): ')))
+    
+    return ilist
+
+def getMax(ilist):
+    print('Max element: ' , max(ilist))
